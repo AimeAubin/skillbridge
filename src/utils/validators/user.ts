285ae9f82b531d6eee0a541c339579dfc7c5552b
Irakoze Aimé Aubin <aubin.irakoze@amalitech.com>
2024-11-22 09:55:22 +0200

    feat: add auth UI

---
+++ src/utils/validators/user.ts
@@ -0,0 +1,49 @@
+import { z } from "zod";
+
+export const LoginSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email("Email must be a valid email"),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .refine(
+      (value) =>
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?\s])[A-Za-z\d!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?\s]*$/.test(
+          value ?? "",
+        ),
+      "Password must contain at least 1 uppercase, 1 lowercase, 1 number and 1 special character",
+    ),
+  code: z.optional(z.string()),
+});
+
+export const RegisterSchema = z.object({
+  name: z
+    .string({
+      required_error: "Name is required",
+    })
+    .min(2, {
+      message: "Name must be at least 2 characters long",
+    }),
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email("Email must be a valid email"),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .refine(
+      (value) =>
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?\s])[A-Za-z\d!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?\s]*$/.test(
+          value ?? "",
+        ),
+      "Password must contain at least 1 uppercase, 1 lowercase, 1 number and 1 special character",
+    ),
+});
