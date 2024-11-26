8f07eda4b1197299dd204fa7cd7201d31a2a1603
Irakoze Aimé Aubin <aubin.irakoze@amalitech.com>
2024-11-26 11:13:16 +0200

    feat: add login functionality

---
+++ src/actions/login.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { signIn } from "@/server/auth";
+import { db } from "@/server/db";
+import { LoginSchema } from "@/utils/validators/user";
+import { AuthError } from "next-auth";
+import { z } from "zod";
+
+export const login = async (
+  values: z.infer<typeof LoginSchema>,
+  callbackUrl?: string | null,
+) => {
+  const validateFields = LoginSchema.safeParse(values);
+
+  if (!validateFields.success) {
+    return { error: "Invalid fields." };
+  }
+
+  const { email, password } = validateFields.data;
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!existingUser?.email || !existingUser?.password) {
+    return { error: "Invalid credentials. Please try again." };
+  }
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: callbackUrl ?? "/dashboard",
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials. Please try again." };
+        default:
+          return { error: "Something went wrong. Please try again." };
+      }
+    }
+
+    throw error;
+  }
+};
--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -3,13 +3,30 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CardWrapper } from "@/components/auth/card-wrapper";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { useForm } from "react-hook-form";
 import { LoginSchema } from "@/utils/validators/user";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { useSearchParams } from "next/navigation";
+import { useTransition, useState } from "react";
+import { login } from "@/actions/login";
+import { FormError } from "@/components/misc/form-error";
+import { FormSuccess } from "@/components/misc/form-success";
 
 export const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+  const [isPending, startTransition] = useTransition();
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
     defaultValues: {
@@ -18,8 +35,23 @@ export const LoginForm = () => {
     },
   });
 
-  const onSubmit = () => {
-    //TODO: Add onSubmit function
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    setError("");
+    setSuccess("");
+
+    startTransition(() => {
+      login(values, callbackUrl)
+        .then((data) => {
+          if (data?.error) {
+            form.reset();
+            setError(data.error);
+          }
+
+          form.reset();
+          setSuccess("User logged in successfully.");
+        })
+        .catch(() => setError("Something went wrong!"));
+    });
   };
 
   return (
@@ -63,6 +95,8 @@ export const LoginForm = () => {
               )}
             />
           </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
           <Button type="submit" className="w-full">
             Create an account
           </Button>
