1dea11765cd6bcc57dc37f95812f5ca4221bbc85
Irakoze Aimé Aubin <aubin.irakoze@amalitech.com>
2024-11-28 09:54:29 +0200

    feat: add profile form to edit profile

--- src/components/dashboardLayout.tsx
+++ src/components/dashboardLayout.tsx
@@ -25,7 +25,7 @@ export default function DashboardLayout({
 
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar collapsible="icon" />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2">
           <div className="flex items-center gap-2 px-4">
